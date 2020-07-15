d763381574899aa3be955055231ab956d2eaccf2
flaaaps <julianwening4@gmail.com>
2020-07-15 22:13:38 +0200

    Add request limit + error output

--- index.js
+++ index.js
@@ -1,18 +1,42 @@
 const express = require('express')
 const cors = require('cors')
+const monk = require('monk')
 const app = express()
+const rateLimit = require('express-rate-limit')
+
+// db stuff
 
 app.use(cors())
 
 app.use(express.json())
 
 app.get('/', (req, res) => {
-    res.send("Hello World")
+    res.send("Everything working fine")
 })
 
+function isValid(feedback) {
+    return feedback.title && feedback.title.toString().trim() !== '' &&
+        feedback.message && feedback.message.toString().trim() !== ''
+}
+app.use(rateLimit({
+    windowMs: 5 * 60 * 1000, // every 5 minutes
+    max: 5,
+    message: {
+        status: 429,
+        msg: 'Too many requests'
+    }
+}))
 app.post('/', (req, res) => {
-    console.log(req.body)
-    res.send(req.body)
+    if (isValid(req.body)) {
+        console.log(req.body)
+        res.send(req.body)
+    } else {
+        res.status(422)
+        res.json({
+            msg: "Please enter a title and a message.",
+            status: 422
+        })
+    }
 })
 
 app.listen(3000, () => {
--- public/client.js
+++ public/client.js
@@ -1,6 +1,8 @@
 const form = document.querySelector('#suggestion-form')
 const feedbackSelect = document.getElementById('feedback')
 const container = document.querySelector('.container')
+const warningCont = document.getElementById('warning')
+const submitBtn = document.getElementById('submit-btn')
 
 // submit suggestion
 form.addEventListener('submit', event => {
@@ -12,7 +14,7 @@ form.addEventListener('submit', event => {
     const message = formData.get('message')
     const type = feedbackSelect.options[feedbackSelect.selectedIndex].value;
 
-    const suggestion = {
+    const feedback = {
         email,
         type,
         title,
@@ -23,20 +25,50 @@ form.addEventListener('submit', event => {
         headers: {
             'Content-Type': 'application/json',
         },
-        body: JSON.stringify(suggestion),
+        body: JSON.stringify(feedback),
     })
-        .then(response => response.json())
-        .then(suggestion => {
-            console.log('Success:', suggestion);
-            form.style.display = "none"
-            container.innerHTML += `<div class="alert alert-success" role="alert">
+        .then(response => {
+            if (response.ok) {
+                response.json()
+                    .then(feedback => {
+                        console.log('Success:', feedback);
+                        container.style.display = "none"
+                        warningCont.innerHTML = ''
+                        warningCont.innerHTML += `<div class="alert alert-success mt-5" role="alert">
                                     <h4 class="alert-heading">Well done!</h4>
-                                    <p>Your <span>${suggestion.type}</span> with the title <span>"${suggestion.title.replace('<', '&lt;').replace('>', '&gt;')}"</span> was successfuly sent!</p>
+                                    <p>Your <span>${feedback.type}</span> with the title <span>"${feedback.title.replace('<', '&lt;').replace('>', '&gt;')}"</span> was successfully sent!</p>
                                     <hr>
                                     <a href="" class="mb-0">Write a new ticket</a>
                                     </div>`
+                        setTimeout(function () {
+                            container.style.display = ''
+                            warningCont.innerHTML = ''
+                        }, 4500)
+                        form.reset()
+                    })
+                    .catch((error) => {
+                        console.error('Error:', error);
+                    });
+            } else {
+                response.json()
+                    .then(err => {
+                        if (err.status === 429) {
+                            submitBtn.disabled = true
+                            setTimeout(function () {
+                                warningCont.innerHTML = ""
+                            }, 300000)
+                        }
+                        setTimeout(function () {
+                            submitBtn.disabled = false
+                        }, 30000)
+                        warningCont.innerHTML = ''
+                        setTimeout(function () {
+                            warningCont.innerHTML += `<div class="alert alert-danger" role="alert">
+                                                        ${err.msg}
+                                                  </div>`
+                        }, 50)
+                    })
+            }
         })
-        .catch((error) => {
-            console.error('Error:', error);
-        });
+    event.preventDefault()
 })
