5de760a01f7a6760a86999f62004a68275768aa4
moritz <theincxption@gmail.com>
2020-07-17 19:37:44 +0200

    Add the ability to provide attachments with a feedback

--- public/js/submit.js
+++ public/js/submit.js
@@ -4,83 +4,121 @@ const languageSelect = document.getElementById('language')
 const container = document.querySelector('.container')
 const warningCont = document.getElementById('warning')
 const submitBtn = document.getElementById('submit-btn')
+const attachmentsInput = document.getElementById("attachments")
 
 // submit suggestion
-form.addEventListener('submit', event => {
-    event.preventDefault()
-    var editor_content = quill.container.firstChild.innerHTML
+form.addEventListener('submit', async function(event) {
+   event.preventDefault()
+   const formData = new FormData(form)
+   const email = formData.get('email')
+   const title = formData.get('subject')
+   const message = quill.container.firstChild.innerHTML
+   const type = feedbackSelect.options[feedbackSelect.selectedIndex].value
+   const lang = languageSelect.options[languageSelect.selectedIndex].value
+   const attachments = []
 
-    console.log(editor_content)
-    const formData = new FormData(form)
-    const email = formData.get('email')
-    const title = formData.get('subject')
-    const message = editor_content
-    const type = feedbackSelect.options[feedbackSelect.selectedIndex].value
-    const lang = languageSelect.options[languageSelect.selectedIndex].value
+   for (let file of attachmentsInput.files) {
+      let link = await upload(file)
+      attachments.push(link)
+   }
 
-    const feedback = {
-        email,
-        type,
-        lang,
-        title,
-        message
-    }
-    fetch('http://localhost:3000', {
-        method: 'POST', // or 'PUT'
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(feedback),
-    })
-        .then(response => {
-            if (response.ok) {
-                response.json()
-                    .then(feedback => {
-                        console.log('Success:', feedback);
-                        container.style.display = "none"
-                        warningCont.innerHTML = ''
-                        warningCont.innerHTML += `<div class="alert alert-success mt-5" role="alert">
+   const feedback = {
+      email,
+      type,
+      lang,
+      title,
+      message
+   }
+
+   if (attachments.length > 0) {
+      feedback.attachments = attachments
+   }
+
+   fetch('http://localhost:3000', {
+      method: 'POST', // or 'PUT'
+      headers: {
+         'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(feedback),
+   }).then(response => {
+      if (response.ok) {
+         response.json()
+            .then(feedback => {
+               console.log('Success:', feedback);
+               container.style.display = "none"
+               warningCont.innerHTML = ''
+               warningCont.innerHTML += `<div class="alert alert-success mt-5" role="alert">
                                     <h4 class="alert-heading">Well done!</h4>
                                     <p>Your <span>${feedback.type}</span> with the title <span>"${feedback.title.replace('<', '&lt;').replace('>', '&gt;')}"</span> was successfully sent!</p>
                                     <hr>
                                     <a href="" class="mb-0">Write a new ticket</a>
                                     </div>`
-                        setTimeout(function () {
-                            container.style.display = ''
-                            warningCont.innerHTML = ''
-                        }, 4500)
-                        form.reset()
-                        quill.container.firstChild.innerHTML = ""
-                    })
-                    .catch((error) => {
-                        console.error('Error:', error);
-                    });
-            } else {
-                response.json()
-                    .then(err => {
-                        if (err.status === 429) {
-                            submitBtn.disabled = true
-                            setTimeout(function () {
-                                warningCont.innerHTML = ""
-                            }, 300000)
-                        }
-                        setTimeout(function () {
-                            submitBtn.disabled = false
-                        }, 30000)
-                        warningCont.innerHTML = ''
-                        container.removeChild(container.childNodes[0])
-                        setTimeout(function () {
-                            const div = document.createElement('div')
-                            div.classList.add('alert', 'alert-danger')
-                            div.setAttribute('role', 'alert')
-                            div.textContent = err.msg
-                            // warningCont.innerHTML += `<div class="alert alert-danger" role="alert">
-                            //                             ${err.msg}
-                            //                      </div>`
-                            container.insertBefore(div, container.firstChild);
-                        }, 50)
-                    })
-            }
-        })
-    event.preventDefault()
+               setTimeout(function () {
+                  container.style.display = ''
+                  warningCont.innerHTML = ''
+               }, 4500)
+               form.reset()
+               quill.container.firstChild.innerHTML = ""
+            })
+            .catch((error) => {
+               console.error('Error:', error);
+            });
+      } else {
+         response.json()
+            .then(err => {
+               if (err.status === 429) {
+                  submitBtn.disabled = true
+                  setTimeout(function () {
+                     warningCont.innerHTML = ""
+                  }, 300000)
+               }
+               setTimeout(function () {
+                  submitBtn.disabled = false
+               }, 30000)
+               warningCont.innerHTML = ''
+               container.removeChild(container.childNodes[0])
+               setTimeout(function () {
+                  const div = document.createElement('div')
+                  div.classList.add('alert', 'alert-danger')
+                  div.setAttribute('role', 'alert')
+                  div.textContent = err.msg
+                  // warningCont.innerHTML += `<div class="alert alert-danger" role="alert">
+                  //                             ${err.msg}
+                  //                      </div>`
+                  container.insertBefore(div, container.firstChild);
+               }, 50)
+            })
+      }
+   })
+   event.preventDefault()
 })
+
+function upload(input) {
+   const name = input.name;
+
+   if (!name.endsWith(".png")
+      && !name.endsWith(".jpg")
+      && !name.endsWith(".jpeg")
+      && !name.endsWith(".gif"))
+      return false
+
+   const url = "https://api.imgur.com/3/image"
+   const reader = new FileReader()
+
+   return new Promise(function (resolve, reject) {
+      reader.readAsDataURL(input)
+      reader.onload = () => {
+         const formData = new FormData();
+         formData.append("image", reader.result.split(",")[1])
+
+         fetch(url, {
+            headers: {
+               "Authorization": "Client-ID c78854cfb8f4e5d"
+            },
+            body: formData,
+            method: "POST"
+         }).then(response => response.json())
+            .then(response => resolve(response.data.link))
+      }
+   })
+}
--- public/js/view.js
+++ public/js/view.js
@@ -3,71 +3,94 @@ const cont = document.querySelector(".container");
 
 // TODO: edit script function so that only script tags are replaced
 function escape(msg) {
-  return msg.replace("<", "&lt;").replace(">", "&gt;");
+   return msg.replace("<", "&lt;").replace(">", "&gt;");
 }
 
 function capitalizeFirstLetter(string) {
-  return string.charAt(0).toUpperCase() + string.slice(1);
+   return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
 function createElmt(html) {
-  var div = document.createElement("div");
-  div.innerHTML = escape(html);
-  return div.childNodes[0];
+   var div = document.createElement("div");
+   div.innerHTML = escape(html);
+   return div.childNodes[0];
 }
 
 fetch(`http://localhost:3000/id?id=${feedbackId}`)
-  .then((response) => response.json())
-  .then((response) => {
-    const result = response[0];
-    if (result === undefined) {
-      // Id not found
-      document.title = "Not found"
-      const div = document.createElement('div')
-      div.classList.add('error')
-      const heading = document.createElement('h1')
-      heading.textContent = 'Whoops.'
+   .then((response) => response.json())
+   .then((response) => {
+      const result = response[0];
+      if (result === undefined) {
+         // Id not found
+         document.title = "Not found"
+         const div = document.createElement('div')
+         div.classList.add('error')
+         const heading = document.createElement('h1')
+         heading.textContent = 'Whoops.'
 
-      const subHeading = document.createElement('h4')
-      subHeading.textContent = `A feedback with the id ${response.id} does not exist.`
+         const subHeading = document.createElement('h4')
+         subHeading.textContent = `A feedback with the id ${response.id} does not exist.`
 
-      // Implement back to overview
+         // Implement back to overview
 
-      div.appendChild(heading)
-      div.appendChild(subHeading)
+         div.appendChild(heading)
+         div.appendChild(subHeading)
 
-      cont.appendChild(div)
+         cont.appendChild(div)
 
-    } else {
-      // display id page
-      document.title = escape(`${result.title} | ${capitalizeFirstLetter(result.type)}`);
+      } else {
+         // display id page
+         document.title = escape(`${result.title} | ${capitalizeFirstLetter(result.type)}`);
 
-      const details = document.createElement('div')
-      details.classList.add('details')
+         const details = document.createElement('div')
+         details.classList.add('details')
 
-      const h2 = document.createElement("h2");
-      h2.appendChild(createElmt(result.title));
+         const h2 = document.createElement("h2");
+         h2.appendChild(createElmt(result.title));
 
-      const lang = document.createElement('div')
-      lang.classList.add('lang')
+         const lang = document.createElement('div')
+         lang.classList.add('lang')
 
-      const flag = document.createElement('img')
-      flag.src = result.lang === 'en' ? 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADwAAAA8CAMAAAANIilAAAAAhFBMVEUAAADvjYbwfHSKYpnt7/GKYpp4hcrr7/HwkYnvq6WAd7Xvx8aHaqShaJDLsMOic5vykYzyj4r0Qzbs7/E/UbXvmJPybmTuxMJ7h8zBp77///+HktFvfMfb3/FLW7nz9Pu3vuNjccNsTZSrs99qeMSTndXn6fbP1O3O0+xrXKbNrr9rbbgEiDpcAAAAEnRSTlMAcMDgfuDAgbCgzI7Y2J6UPDtbvABhAAAAz0lEQVRIx+2TSxKCMBBEBQEB/zBgTEIUERS9//1UilUkMRW0KKy8Re/epqdnYjD8L+upiKUtwWpkF0ScIgn2V2QCwAelSnKYZ4cjcFFWVQn3nYR5I3uQYQRcMIwZnGMJTisjRIELRoiiLEBFdqGzMVYqtn3BV8YFfdamJhe44KKpTUFevI5K3oKgvE4l+P0L8wQLU5NFC1ORQ+HC6kRC0LYtWFivl/y5vNqLuH1+yVmshTOwbEda2APLfqqFP+ZTBYkWwZjvvLG02E4MBkMXD5MAkJrGRHOLAAAAAElFTkSuQmCC' : 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADwAAAA8BAMAAADI0sRBAAAALVBMVEUAAABTV1z/mAVEWWT/wAf/PABEWmT/wQZlVFH/nARFWmL/wgX/PQBFWmT/wQcERp1ZAAAADHRSTlMAz8+/v4CAgHR0YGBk1e17AAAARklEQVQ4y2MYBbQAi41xAiugdO5dnODaqDSZ0o2COIEEUDrmDE5wdFSaNtKTlHACTaB03Tuc4PmoNJnSW1xwAm+GUUADAAA0cjjQYLRUuAAAAABJRU5ErkJggg=='
+         const flag = document.createElement('img')
+         flag.src = result.lang === 'en' ? 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADwAAAA8CAMAAAANIilAAAAAhFBMVEUAAADvjYbwfHSKYpnt7/GKYpp4hcrr7' +
+            '/HwkYnvq6WAd7Xvx8aHaqShaJDLsMOic5vykYzyj4r0Qzbs7/E/UbXvmJPybmTuxMJ7h8zBp77///+HktFvfMfb3/FLW7nz9Pu3vuNjccNsTZSrs99qeMSTndXn6fbP1O3O0+xrXKb' +
+            'Nrr9rbbgEiDpcAAAAEnRSTlMAcMDgfuDAgbCgzI7Y2J6UPDtbvABhAAAAz0lEQVRIx+2TSxKCMBBEBQEB/zBgTEIUERS9//1UilUkMRW0KKy8Re/epqdnYjD8L+upiKUtwWpkF0ScI' +
+            'gn2V2QCwAelSnKYZ4cjcFFWVQn3nYR5I3uQYQRcMIwZnGMJTisjRIELRoiiLEBFdqGzMVYqtn3BV8YFfdamJhe44KKpTUFevI5K3oKgvE4l+P0L8wQLU5NFC1ORQ+HC6kRC0LYtWFi' +
+            'vl/y5vNqLuH1+yVmshTOwbEda2APLfqqFP+ZTBYkWwZjvvLG02E4MBkMXD5MAkJrGRHOLAAAAAElFTkSuQmCC'
+            : 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADwAAAA8BAMAAADI0sRBAAAALVBMVEUAAABTV1z/mAVEWWT/wAf/PABEWmT/wQZlVFH/nARFWmL/wgX/PQBFWmT/wQ' +
+            'cERp1ZAAAADHRSTlMAz8+/v4CAgHR0YGBk1e17AAAARklEQVQ4y2MYBbQAi41xAiugdO5dnODaqDSZ0o2COIEEUDrmDE5wdFSaNtKTlHACTaB03Tuc4PmoNJnSW1xwAm+GUUADAAA0' +
+            'cjjQYLRUuAAAAABJRU5ErkJggg=='
 
-      lang.appendChild(flag)
+         const attachments = document.createElement("ul")
+         if (result.attachments) {
+            let index = 1
+            for (let link of result.attachments) {
+               let entry = document.createElement("li")
+               let a = document.createElement("a")
+               a.href = link
+               a.textContent = "Attachment #" + index
+               entry.appendChild(a)
+               attachments.appendChild(entry)
 
-      const message = document.createElement("p");
-      message.innerHTML = result.message;
-      // message.appendChild(createElmt(result.message));
+               index++
+            }
+         }
 
-      const date = document.createElement("p");
-      date.innerText = moment(result.created).fromNow();
-      date.classList.add('created')
+         lang.appendChild(flag)
 
-      details.appendChild(h2);
-      details.appendChild(lang)
-      cont.appendChild(details)
-      cont.appendChild(date);
-      cont.appendChild(message);
-    }
-  });
+         const message = document.createElement("p");
+         message.innerHTML = result.message;
+         // message.appendChild(createElmt(result.message));
+
+         const date = document.createElement("p");
+         date.innerText = moment(result.created).fromNow();
+         date.classList.add('created')
+
+         details.appendChild(h2);
+         details.appendChild(lang)
+         cont.appendChild(details)
+         cont.appendChild(date);
+         cont.appendChild(message);
+         cont.appendChild(attachments)
+      }
+   });
