dd82583630f6027c5d9d2e955434f2e578faf17d
flaaaps <julianwening4@gmail.com>
2020-08-11 22:58:20 +0200

    Set position of footer based on content height

--- public/js/main.js
+++ public/js/main.js
@@ -4,7 +4,7 @@ function scrollToTop() {
     $('html, body').animate({ scrollTop: 0 }, 400), closeMenu();
 }
 
-window.addEventListener('load', function () {
+document.addEventListener('load', function () {
     // Close the nav menu
 
     function closeMenu() {
@@ -25,4 +25,10 @@ window.addEventListener('load', function () {
         }, 500);
     }
 
+    // Set position of footer based on content height
+    if (document.querySelector('.cont-view').clientHeight > 500) {
+        console.log(document.querySelector('.cont-view').clientHeight)
+        document.querySelector('#footer').style.setProperty("position", "relative", "important");
+        console.log(document.querySelector('#footer.view'))
+    }
 });
